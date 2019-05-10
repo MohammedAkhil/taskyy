08c783ab99f30643b2c0d67a6de7fef01f407226
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-10 12:21:05 +0530

    get random image from unsplash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_web/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() => runApp(MyApp());
 
@@ -7,11 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tasky',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: "Taskyy",
+      ),
     );
   }
 }
@@ -23,39 +27,74 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
-            ),
-          ],
+        body: Stack(
+      children: <Widget>[backgroundImage(), appBar()],
+    ));
+  }
+
+  Widget loader() => Center(
+        child: CircularProgressIndicator(),
+      );
+
+  Widget appBar() => Container(
+        margin: EdgeInsets.only(top: 16, left: 8),
+        height: 54,
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 48),
+          ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+
+  Widget backgroundImage() => FutureBuilder<String>(
+        future: getUnsplashImage(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return loader();
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              return Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.8),
+                      backgroundBlendMode: BlendMode.colorBurn,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://images.unsplash.com/photo-1555920141-738465c260b3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjcwNzQyfQ"),
+                          fit: BoxFit.cover)));
+          }
+          return null; // unreachable
+        },
+      );
+
+  Future<String> getUnsplashImage() async {
+    String url = "https://api.unsplash.com/photos/random";
+    Map<String, String> headers = {
+      "authorization":
+          "Client-ID 1e2bb9373792637054b9c00073b59a615297e2c07c08fae1d22b0d47fbbc424d"
+    };
+    try {
+      var response = await http.get(
+        url,
+        headers: headers,
+      );
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        return jsonResponse["urls"]["regular"];
+      } else {
+        return Future.error("Failed");
+      }
+    } catch (err) {
+      print(err);
+      return Future.error(err);
+    }
   }
 }
