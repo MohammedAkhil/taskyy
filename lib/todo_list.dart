affd2b24bb6749450cafa7924c2edca1a7d84876
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-14 11:41:45 +0530

    add list view for todo widget

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,8 +1,29 @@
+import 'package:flutter_web/material.dart';
+import 'data/Todo.dart';
+import 'todo.dart';
+
 class TodoList extends StatelessWidget {
+  final List<Todo> todoList;
+  final Function onSave;
+
+  TodoList({@required this.todoList, this.onSave});
+
   @override
   Widget build(BuildContext context) {
+    print(todoList.length);
     return Container(
-      CardV
+      margin: EdgeInsets.all(8),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return buildTodo(index);
+        },
+        shrinkWrap: true,
+        itemCount: todoList.length,
+      ),
     );
+  }
+
+  Widget buildTodo(int index) {
+    return TodoWidget(this.todoList[index], onSave: this.onSave,);
   }
 }
