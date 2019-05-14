f084003d8e21958d0e76fc20def10495ae150348
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-14 12:38:49 +0530

    convert stateless to state widget

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,31 +1,64 @@
 import 'package:flutter_web/material.dart';
 import 'data/Todo.dart';
 
-class TodoWidget extends StatelessWidget {
+class TodoWidget extends StatefulWidget {
   final Todo todo;
   final Function onSave;
-  final TextEditingController controller = TextEditingController();
+  final Function onEdit;
 
-  TodoWidget(this.todo, {this.onSave});
+  TodoWidget(this.todo, {this.onSave, this.onEdit});
 
   @override
-  Widget build(BuildContext context) => ListTile(
-        title: todo.isEditing
-            ? Row(
-                children: <Widget>[textInput(), doneButton()],
-              )
-            : Text(todo.title),
-      );
+  _TodoWidgetState createState() => _TodoWidgetState();
+}
+
+class _TodoWidgetState extends State<TodoWidget> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.text = widget.todo.title;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: widget.todo.isEditing
+          ? Row(
+              children: <Widget>[textInput(), doneButton()],
+            )
+          : Row(
+              children: <Widget>[
+                Expanded(child: Text(widget.todo.title)),
+                editButton()
+              ],
+            ),
+    );
+  }
 
   Widget textInput() => Expanded(
         child: TextField(
           controller: controller,
+          autofocus: true,
         ),
       );
 
   Widget doneButton() => IconButton(
       icon: Icon(Icons.check),
       onPressed: () {
-        onSave(controller.text, todo.id);
+        widget.onSave(controller.text, widget.todo.id);
+      });
+
+  Widget editButton() => IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        this.widget.onEdit(this.widget.todo.id);
       });
 }
